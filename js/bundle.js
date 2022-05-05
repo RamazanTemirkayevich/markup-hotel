9198541cb151f2e8948ae7c2af03750a5acce7fc
Family <family@MacBook-Air-Family.local>
2022-05-05 23:44:42 +0300

    first commit

---
+++ js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+window.addEventListener('DOMContentLoaded', () => {
+    
+});
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
